6725c4822516a8333eab251511cb6afb8c75c3e3
Marctupadre <116338801+Marctupadre@users.noreply.github.com>
2026-02-13 12:04:30 +0100

    using new input system with WASD
    aaaaaaaaaaaaaaaaaaa

--- Assets/Scripts/BallGame/BallInput.cs
+++ Assets/Scripts/BallGame/BallInput.cs
@@ -6,12 +6,36 @@ using UnityEngine;
 public class BallInput : MonoBehaviour
 {
     BallController controller;
+    BallControls input;
+
+    Vector2 moveInput;
+    bool jumpPressed;
+
+    void Awake()
+    {
+        input = new BallControls();
+
+        input.Ball.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        input.Ball.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        input.Ball.Jump.performed += ctx => jumpPressed = true;
+    }
     private void Start()
     {
         controller = GetComponent<BallController>();
     }
     void Update()
     {
+        controller.Move(moveInput);
 
+        if (jumpPressed)
+        {
+            controller.Jump();
+            jumpPressed = false;
+        }
     }
+
+    void OnEnable() => input.Enable();
+
+    void OnDisable() => input.Disable();
 }
